070e46821774c8de8c4b022c71d3f6fa068c5ea6
Praneeth1606 <94961000+Praneeth1606@users.noreply.github.com>
2025-04-10 14:17:32 +0530

    .

--- econtest/settings.py
+++ econtest/settings.py
@@ -18,8 +18,8 @@ SECRET_KEY = getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["44.201.105.224", "localhost", "127.0.0.1"]
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ["44.201.105.224", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
