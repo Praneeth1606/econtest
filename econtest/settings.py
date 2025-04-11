38f9ff6415bef75a8d3b02ae8262aedde4200136
Praneeth1606 <94961000+Praneeth1606@users.noreply.github.com>
2025-04-11 15:58:28 +0530

    .

--- econtest/settings.py
+++ econtest/settings.py
@@ -16,10 +16,10 @@ load_dotenv(os.path.join(Path(__file__).resolve().parent, '.env'))
 SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-# ALLOWED_HOSTS = ["44.201.105.224", "localhost", "127.0.0.1"]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["44.201.105.224", "localhost", "127.0.0.1"]
+# ALLOWED_HOSTS = []
 
 
 # Application definition
