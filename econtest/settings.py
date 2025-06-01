9c77806398644a8ce5d147c4354bd8593f06e29c
Praneeth1606 <94961000+Praneeth1606@users.noreply.github.com>
2025-06-01 18:33:29 +0530

    Update settings.py

--- econtest/settings.py
+++ econtest/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["50.19.135.151", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["50.19.135.151", "localhost", "127.0.0.1", ".vercel.app"]
 # ALLOWED_HOSTS = []
 
 
